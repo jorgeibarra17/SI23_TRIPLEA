3da8861039e35a9114ac8d7f73d85e9912858af8
AdrianAG11 <adrian.aguirre.cetys@gmail.com>
2023-06-02 18:16:11 -0700

    Avance algo

--- proyectos_finales/traffic_sign_recognition/network.py
+++ proyectos_finales/traffic_sign_recognition/network.py
@@ -6,40 +6,38 @@ from pathlib import Path
 file_path = Path(__file__).parent.absolute()
 
 class Network(nn.Module):
-    def __init__(self, num_classes):
-        super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+    def __init__(self, input_dim, num_classes):
+        super().__init__()
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        #Red neuronal
+        self.conv1 = nn.Conv2d(3, input_dim, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc1 = nn.Linear(16, 128)
         self.fc2 = nn.Linear(128, num_classes)
+        self.to(self.device)
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = x.view(x.size(0), 64 * 8 * 8)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
-
-def main():
-    # Definir el número de clases en la base de datos
-    num_classes = 43
-
-    # Crear una instancia del modelo
-    net = Network(num_classes)
-
-    # Imprimir la arquitectura de la red neuronal
-    print(net)
-
-    # Ejecutar una pasada hacia adelante con datos aleatorios
-    output = net(torch.rand(1, 3, 32, 32))
-    print(output)
-
-if __name__ == "__main__":
-    main()
+    def forward(self, x: torch.Tensor):
+        #Fowardpass
+        feature_map = self.conv1(x)
+        feature_map = F.relu(feature_map)
+        feature_map = self.conv2(feature_map)
+        feature_map = self.pool(feature_map)
+        
+        features = torch.flatten(feature_map, start_dim=-1)
+        features = self.fc1(features)
+        features = F.relu(features)
+        features = self.fc2(features)
+        
+        return features
+    
+    def predict(self, x: torch.Tensor) -> torch.Tensor:
+        with torch.inference_mode():
+            return self.forward(x)
+        
+    def save_model(self, model_name: str):
+        models_path = file_path/'models'/model_name
+        torch.save(models_path)
+        
+    def load_model(self, model_name: str):
+        torch.load(model_name)        
--- proyectos_finales/traffic_sign_recognition/train.py
+++ proyectos_finales/traffic_sign_recognition/train.py
@@ -2,22 +2,37 @@ from dataset import get_dataloaders
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 from tqdm import tqdm
 from network import Network
 from plot_losses import PlotLosses
 from pathlib import Path
 
+def eval(val_loader, net, cost_function):
+    val_loss = []
+    for i, batch in enumerate(val_loader, 0):
+        batch_imgs = batch['transformed']
+        batch_labels = batch['label']
+        device = net.device
+        batch_imgs = batch_imgs.to(device)
+        batch_labels = batch_labels.to(device)
+        with torch.inference_mode():
+            predictions = net(batch_imgs)
+            loss = cost_function(predictions, batch_labels)
+            val_loss.append(loss.item())
+    return np.mean(val_loss)        
+
 def train():
     # Hyperparámetros
-    learning_rate = 0.001
+    learning_rate = 1e-5
     num_epochs = 50
     batch_size = 64
 
     # Obtén los dataloaders de entrenamiento y validación
-    train_loader, val_loader = get_dataloaders(batch_size)
-
+    train_dataloader, val_dataloader = get_dataloaders(batch_size)
+    
     # Crea la instancia de tu modelo y mueve los parámetros a la GPU (si está disponible)
-    model = Network(num_classes=43)
+    model = Network(input_dim=32, num_classes=42)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
@@ -27,55 +42,43 @@ def train():
 
     # Crea el objeto para visualizar las curvas de pérdida
     plotter = PlotLosses()
-
+    running_loss = []
+    best_epoch_loss = np.inf
     # Entrenamiento
     for epoch in range(num_epochs):
-        train_loss = 0.0
-        val_loss = 0.0
-
         # Bucle de entrenamiento
-        model.train()
-        for images, labels in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs} - Training"):
-            images = images.to(device)
-            labels = labels.to(device)
+        for i, batch in enumerate(tqdm(train_dataloader, desc=f"Epoch {epoch}")):
+            batch_images, batch_labels = batch
+            batch_images = batch_images.to(device)
+            batch_labels = batch_labels.to(device)
+            
+            #Zero grad
             optimizer.zero_grad()
 
             # Realiza el forward pass y calcula el costo
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+            predictions = model(batch_images)
+            loss = criterion(predictions, batch_labels)
 
             # Realiza la retropropagación y actualiza los parámetros
             loss.backward()
             optimizer.step()
 
-            train_loss += loss.item() * images.size(0)
-
-        # Bucle de validación
-        model.eval()
-        with torch.no_grad():
-            for images, labels in tqdm(val_loader, desc=f"Epoch {epoch+1}/{num_epochs} - Validation"):
-                images = images.to(device)
-                labels = labels.to(device)
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                val_loss += loss.item() * images.size(0)
-
-        # Calcula las pérdidas promedio por epoch
-        train_loss /= len(train_loader.dataset)
-        val_loss /= len(val_loader.dataset)
+            running_loss.append(loss.item())
+        #Costo promedio de entrenamiento
+        train_loss = np.mean(running_loss)
+        #Costo promedio de validacion
+        val_loss = eval(val_dataloader, model, criterion)
 
         # Muestra las pérdidas
         tqdm.write(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f} - Val Loss: {val_loss:.4f}")
 
-        # Actualiza el plotter de las curvas de pérdida
-        plotter.update(train_loss, val_loss)
-        plotter.plot()
-
-    # Guarda el modelo después de finalizar el entrenamiento
-    save_path = Path("modelo.pth")
-    torch.save(model.state_dict(), save_path)
-    tqdm.write(f"Modelo guardado en {save_path}")
-
+       # TODO guarda el modelo si el costo de validación es menor al mejor costo de validación
+        if val_loss < best_epoch_loss :
+            PATH = "modelo_PF.pt"
+            torch.save(model.state_dict(), PATH)
+            tqdm.write(f"Modelo guardado en {save_path}")
+        plotter.on_epoch_end(epoch, train_loss, val_loss)
+    plotter.on_train_end()
 
 if __name__ == "__main__":
     train()
